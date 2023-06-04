784bbbd540cc67b8119f1a923a52dad5285c511f
himself65 <himself65@outlook.com>
2023-06-05 02:08:08 +0800

    feat: add vscode preload

--- electron/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('apis', {
-  // bypass
-  'com.calculator.aPlusB': (...args) => ipcRenderer.invoke(
-    'com.calculator.aPlusB', ...args)
-})
